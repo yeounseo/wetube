ecb5ec7a49fd23a5dc124982f02f85e73eb7fbbb
yeounseo <yeoandante@gmail.com>
2021-01-04 22:43:44 +0900

    create contorollers and routes and pug views

---
+++ routers/userRouter.js
@@ -0,0 +1,17 @@
+import express from "express";
+import routes from "../routes";
+import {
+  changePassword,
+  editProfile,
+  userDetail,
+  users,
+} from "../controllers/userController";
+
+const userRouter = express.Router();
+
+userRouter.get(routes.users, users);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.changePassword, changePassword);
+
+export default userRouter;
